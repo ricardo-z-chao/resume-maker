1d477dc0cfccd1dc6d9c94854e441a044e3bc5b8
ricardo <ricardo.z.chao@gmail.com>
2026-01-04 21:50:49 +0800

    fix: lint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,26 +1,29 @@
-import js from '@eslint/js';
-import globals from 'globals';
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginVue from 'eslint-plugin-vue';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import css from '@eslint/css';
 import { defineConfig } from 'eslint/config';
+import globals from 'globals';
 
 export default defineConfig([
   {
-    ignores: ['**/dist/**'],
+    ignores: ['dist/'],
   },
-  tseslint.configs.recommended,
-  pluginVue.configs['flat/essential'],
+  { languageOptions: { globals: globals.browser } },
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts,vue}'],
-    plugins: { js },
-    extends: ['js/recommended'],
+    files: ['**/*.{js,cjs,mjs}'],
+    ...eslint.configs.recommended,
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ['**/*.{ts}'],
+    extends: tseslint.configs.recommended,
     languageOptions: { globals: globals.browser },
   },
   {
     files: ['**/*.vue'],
-    languageOptions: { parserOptions: { parser: tseslint.parser } },
+    extends: pluginVue.configs['flat/vue2-recommended'],
   },
   {
     files: ['**/*.css'],
