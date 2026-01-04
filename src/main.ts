4263b53ff0e7376a5247fcdb9981e9b84c4fbdd8
ricardo <ricardo.z.chao@gmail.com>
2026-01-04 21:55:02 +0800

    chore: add global css

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
-import 'normalize.css';
-
-Vue.config.productionTip = false;
+import '@/styles/index.css';
 
 new Vue({
   render: (h) => h(App),
