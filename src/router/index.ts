66ab85bb8216b299f5e68571017b3fa19bc510e1
ricardo <ricardo.z.chao@gmail.com>
2026-01-06 01:40:29 +0800

    chore: add router config

---
+++ src/router/index.ts
@@ -0,0 +1,40 @@
+import Vue from 'vue';
+import VueRouter, { type RouteConfig } from 'vue-router';
+import HelloPage from '@/layout/HelloPage.vue';
+import ResumeList from '@/layout/ResumeList.vue';
+import { isFirstVisited, markVisited } from '@/utils';
+
+Vue.use(VueRouter);
+
+const routes: RouteConfig[] = [
+  {
+    path: '/hello',
+    component: HelloPage,
+  },
+  {
+    path: '/',
+    component: ResumeList,
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes,
+});
+
+router.beforeEach((to, _from, next) => {
+  if (to.path === '/hello') {
+    next();
+    if (isFirstVisited()) {
+      markVisited();
+    } else {
+      next('/');
+    }
+  } else if (isFirstVisited()) {
+    next('/hello');
+  } else {
+    next();
+  }
+});
+
+export default router;
