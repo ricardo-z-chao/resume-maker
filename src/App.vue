34bae0287502540ccaf95b5add2e7b6c82fd016a
ricardo <ricardo.z.chao@gmail.com>
2026-01-11 02:18:28 +0800

    refact: use defineComponent

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script lang="ts">
-export default {
+import { defineComponent } from 'vue';
+
+export default defineComponent({
   name: 'App',
-};
+});
 </script>
 
 <style scoped></style>
