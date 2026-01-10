cbc56bacfa21b2cb7a904cd02a0a979bd6cf8436
ricardo <ricardo.z.chao@gmail.com>
2026-01-11 02:43:27 +0800

    chore: use ts

--- vue.config.ts
+++ vue.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from '@vue/cli-service';
-import * as process from 'process';
 
 export default defineConfig({
   publicPath: process.env.NODE_ENV === 'production' ? '/resume-maker/' : '/',
