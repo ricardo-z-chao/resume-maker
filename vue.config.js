eb7322eb00a3ab8012e9cf0d806299919d457597
ricardo <ricardo.z.chao@gmail.com>
2026-01-11 01:54:54 +0800

    chore: tailwindcss config

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,15 @@ import * as process from 'process';
 
 export default defineConfig({
   publicPath: process.env.NODE_ENV === 'production' ? '/resume-maker/' : '/',
+  css: {
+    loaderOptions: {
+      postcss: {
+        postcssOptions: {
+          plugins: {
+            '@tailwindcss/postcss': {},
+          },
+        },
+      },
+    },
+  },
 });
