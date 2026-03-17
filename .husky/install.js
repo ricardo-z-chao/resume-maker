1cf3f8aef0b9548b0bed764f280f2babc82a9f42
ricardo <ricardo.z.chao@gmail.com>
2026-03-17 15:54:45 +0800

    chore: husky config

--- .husky/install.js
+++ .husky/install.js
@@ -1,8 +1,8 @@
-import * as process from 'process';
+import * as process from "process";
 
-if (process.env.NODE_ENV === 'production' || process.env.CI === 'true') {
+if (process.env.NODE_ENV === "production" || process.env.CI === "true") {
   process.exit(0);
 }
 
-const husky = (await import('husky')).default;
+const husky = (await import("husky")).default;
 husky();
